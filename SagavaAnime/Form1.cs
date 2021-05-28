27b47d34983f1dd4bb849b9571f4d5c82286b6c7
AIek Ryjhkov <al.pijcov@gmail.com>
2021-05-28 16:05:02 +0500

    Прибрался в коде

--- SagavaAnime/Form1.cs
+++ SagavaAnime/Form1.cs
@@ -16,28 +16,7 @@ namespace SagavaAnime
         {
             InitializeComponent();
         }
-        private int[,] matrix_Mul(int[,] a, int[,] b)
-        {
-            // перемножение двух матриц
-            int[,] res = new int[4, 4]{
-                { 0, 0, 0, 0 },
-                { 0, 0, 0, 0 },
-                { 0, 0, 0, 0 },
-                { 0, 0, 0, 0 }
-            }; 
-            for (int i = 0; i < 4; i++)
-            {
-                for (int j = 0; j < 4; j++)
-                {
-                    res[i, j] = 0;
-                    for (int k = 0; k < 4; k++)
-                    {
-                        res[i, j] += a[i, k] * b[k, j];
-                    }
-                }
-            }
-            return res;
-        }
+        
         private void matrix_Draw(int[,] m)
         {
             Graphics graphics = pictureBox1.CreateGraphics();
@@ -46,56 +25,23 @@ namespace SagavaAnime
 
             // Рациональную матрицу удобно отображать в виде
             // окружностей разного диаметра
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    int k = 2;
+                    int r = m[i, j] * k;
+                    graphics.DrawEllipse(pen, 20 + 80 * i - r, 20 + 80 * j - r, 40 + 2 * r, 40 + 2 * r);
+                }
+            }
+            Brush brush = new SolidBrush(Color.DarkRed);
+            for (int i = 0; i < 4; i++)
+            {
+                int k = 2;
+                int r = m[i, i] * k;
+                graphics.FillEllipse(brush, 20 + 80 * i - r, 20 + 80 * i - r, 40 + 2 * r, 40 + 2 * r);
+            }
             
-            int k = 2;
-            int r = m[0,0] * k;
-            graphics.DrawEllipse(pen, 20 - r, 20 - r, 40 + 2 * r, 40 + 2 * r);
-            r = m[0,1] * k;
-            graphics.DrawEllipse(pen, 100 - r, 20 - r, 40 + 2 * r, 40 + 2 * r);
-            r = m[0,2] * k;
-            graphics.DrawEllipse(pen, 180 - r, 20 - r, 40 + 2 * r, 40 + 2 * r);
-            r = m[0,3] * k;
-            graphics.DrawEllipse(pen, 260 - r, 20 - r, 40 + 2 * r, 40 + 2 * r);
-
-
-            r = m[1,0] * k;
-            graphics.DrawEllipse(pen, 20 - r, 100 - r, 40 + 2 * r, 40 + 2 * r);
-            r = m[1,1] * k;
-            graphics.DrawEllipse(pen, 100 - r, 100 - r, 40 + 2 * r, 40 + 2 * r);
-            r = m[1,2] * k;
-            graphics.DrawEllipse(pen, 180 - r, 100 - r, 40 + 2 * r, 40 + 2 * r);
-            r = m[1,3] * k;
-            graphics.DrawEllipse(pen, 260 - r, 100 - r, 40 + 2 * r, 40 + 2 * r);
-
-
-            r = m[2,0] * k;
-            graphics.DrawEllipse(pen, 20 - r, 180 - r, 40 + 2 * r, 40 + 2 * r);
-            r = m[2,1] * k;
-            graphics.DrawEllipse(pen, 100 - r, 180 - r, 40 + 2 * r, 40 + 2 * r);
-            r = m[2,2] * k;
-            graphics.DrawEllipse(pen, 180 - r, 180 - r, 40 + 2 * r, 40 + 2 * r);
-            r = m[2,3] * k;
-            graphics.DrawEllipse(pen, 260 - r, 180 - r, 40 + 2 * r, 40 + 2 * r);
-
-            r = m[3,0] * k;
-            graphics.DrawEllipse(pen, 20 - r, 260 - r, 40 + 2 * r, 40 + 2 * r);
-            r = m[3,1] * k;
-            graphics.DrawEllipse(pen, 100 - r, 260 - r, 40 + 2 * r, 40 + 2 * r);
-            r = m[3,2] * k;
-            graphics.DrawEllipse(pen, 180 - r, 260 - r, 40 + 2 * r, 40 + 2 * r);
-            r = m[3,3] * k;
-            graphics.DrawEllipse(pen, 260 - r, 260 - r, 40 + 2 * r, 40 + 2 * r);
-
-            Brush brush = new SolidBrush(Color.BlueViolet);
-            
-            r = m[0,0]*k;
-            graphics.FillEllipse(brush, 20 - r, 20 - r, 40 + 2 * r, 40 + 2 * r);
-            r = m[1,1] * k;
-            graphics.FillEllipse(brush, 100 - r, 100 - r, 40 + 2 * r, 40 + 2 * r);
-            r = m[2,2] * k;
-            graphics.FillEllipse(brush, 180 - r, 180 - r, 40 + 2 * r, 40 + 2 * r);
-            r = m[3,3] * k;
-            graphics.FillEllipse(brush, 260 - r, 260 - r, 40 + 2 * r, 40 + 2 * r);
         }
         private void button1_Click(object sender, EventArgs e)
         {
