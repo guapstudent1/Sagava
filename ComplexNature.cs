08dd58b47831c3940a2dd7775aa8760927ade935
AIek Ryjhkov <al.pijcov@gmail.com>
2021-05-28 12:17:53 +0500

    Добавьте файлы проекта.

---
+++ ComplexNature.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Sagava
+{
+    public class ComplexNumber
+    {
+    }
+    public class ComplexNature
+    {
+        int[,] _matrix = new int[4,4] {
+            { 1, 2, 3, 4},
+            { 2, 3, 4, 1},
+            { 3, 4, 1, 2},
+            { 4, 1, 2, 3}
+        };
+    }
+}
