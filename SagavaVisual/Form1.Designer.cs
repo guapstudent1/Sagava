9a63d0779e3f094944c66cb54cbe55e7288d8b02
AIek Ryjhkov <al.pijcov@gmail.com>
2021-05-28 12:17:53 +0500

    НачалоДобавьте файлы проекта.

---
+++ SagavaVisual/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿
+namespace SagavaVisual
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(638, 33);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(138, 44);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Подсчитать собственные числа";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(638, 90);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(137, 53);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Визуализовать матрицу";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Ряд 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Ряд 2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(28, 49);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 15);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Ряд 3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(28, 64);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 15);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Ряд 4";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(180, 19);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(436, 330);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Проект Сагава";
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ SagavaVisual/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace SagavaVisual
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Graphics graphics = pictureBox1.CreateGraphics();
+
+            Pen pen = new Pen(Color.BlueViolet, 2);
+
+            // В случае задания матрицы комплексными числами 
+            // Их удобно отображать в виде роторов
+            graphics.DrawLine(pen, 30, 30, 40, 50);
+            graphics.DrawEllipse(pen, 20,  20, 20, 20);
+
+
+            graphics.DrawEllipse(pen, 60,  20, 20, 20);
+            graphics.DrawEllipse(pen, 100, 20, 20, 20);
+            graphics.DrawEllipse(pen, 140, 20, 20, 20);
+
+            graphics.DrawEllipse(pen, 20,  60, 20, 20);
+            graphics.DrawEllipse(pen, 60,  60, 20, 20);
+            graphics.DrawEllipse(pen, 100, 60, 20, 20);
+            graphics.DrawEllipse(pen, 140, 60, 20, 20);
+
+            graphics.DrawEllipse(pen, 20,  100, 20, 20);
+            graphics.DrawEllipse(pen, 60,  100, 20, 20);
+            graphics.DrawEllipse(pen, 100, 100, 20, 20);
+            graphics.DrawEllipse(pen, 140, 100, 20, 20);
+
+            graphics.DrawEllipse(pen, 20,  140, 20, 20);
+            graphics.DrawEllipse(pen, 60,  140, 20, 20);
+            graphics.DrawEllipse(pen, 100, 140, 20, 20);
+            graphics.DrawEllipse(pen, 140, 140, 20, 20);
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            int[,] _matrix = new int[4, 4] {
+                { 1, 2, 3, 4},
+                { 2, 3, 4, 1},
+                { 3, 4, 1, 2},
+                { 4, 1, 2, 3}
+            };
+            String row1 = _matrix[0, 0].ToString();
+            row1 += " " + _matrix[0, 1].ToString();
+            row1 += " " + _matrix[0, 2].ToString();
+            row1 += " " + _matrix[0, 3].ToString();
+            label1.Text = row1;
+
+            String row2 = _matrix[1, 0].ToString();
+            row2 += " " + _matrix[1, 1].ToString();
+            row2 += " " + _matrix[1, 2].ToString();
+            row2 += " " + _matrix[1, 3].ToString();
+            label2.Text = row2;
+
+            String row3 = _matrix[2, 0].ToString();
+            row3 += " " + _matrix[2, 1].ToString();
+            row3 += " " + _matrix[2, 2].ToString();
+            row3 += " " + _matrix[2, 3].ToString();
+            label3.Text = row3;
+
+            String row4 = _matrix[3, 0].ToString();
+            row4 += " " + _matrix[3, 1].ToString();
+            row4 += " " + _matrix[3, 2].ToString();
+            row4 += " " + _matrix[3, 3].ToString();
+            label4.Text = row4;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Подсчет собственных чисел рациональной матрицы предлагаю сделать самостоятельно
+        }
+    }
+}
